4b3b60f06b3f6a48866c896670adcfab1f4a8448
simon.mcallister.0210 <simon.mcallister.0210@gmail.com>
2022-10-30 14:11:40 +0000

    export types

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { CognitoSrpHelper } from "./cognito-srp-helper";
 
 export * from "./cognito-srp-helper";
 export * from "./constants";
+export * from "./types";
 export * from "./utils";
 export default CognitoSrpHelper;
